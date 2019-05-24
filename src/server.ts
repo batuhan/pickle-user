9127d9940a923f76f960926fa591cadbcbff279d
soner.cirit <sonercirit@gmail.com>
2019-05-24 07:50:56 +0300

    removes comments and inits db connection on server startup

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { ApolloServer, gql, ServerInfo } from "apollo-server";
+import { createConnection } from "typeorm";
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
+// init db connection from ormconfig.json
+createConnection();
+
 const books = [
   {
     author: "J.K. Rowling",
@@ -14,41 +15,26 @@ const books = [
   }
 ];
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
   type Book {
     title: String
     author: String
   }
 
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
   type Query {
     books: [Book]
   }
 `;
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
     books: () => books
   }
 };
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then((serverInfo: ServerInfo) => {
-  // tslint:disable-next-line:no-console
   console.log(`Server ready at ${serverInfo.url}`);
 });
 
