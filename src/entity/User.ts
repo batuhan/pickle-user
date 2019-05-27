63af8012a9df4cc01da491c17ead2c2d564a1f37
sonercirit <sonercirit@gmail.com>
2019-05-27 14:22:26 +0300

    implements min pass lengths and argon2 hashing

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,5 +1,7 @@
-import { Entity, Column } from "typeorm";
+import { Entity, Column, BeforeInsert, BeforeUpdate } from "typeorm";
 import { PickleUserBaseEntity } from "./PickleUserBaseEntity";
+import argon2, { argon2id } from "argon2";
+import config from "../../config.json";
 
 @Entity()
 export class User extends PickleUserBaseEntity {
@@ -8,4 +10,33 @@ export class User extends PickleUserBaseEntity {
 
   @Column()
   password!: string;
+
+  @BeforeInsert()
+  async bI() {
+    this.password = await hash(this.password);
+  }
+
+  @BeforeUpdate()
+  async bU() {
+    this.password = await hash(this.password);
+  }
+}
+
+async function hash(password: string) {
+  if (password) {
+    if (
+      password.length < config.password.minLength ||
+      password.length > config.password.maxLength
+    ) {
+      throw new Error(
+        `Password must be between ${config.password.minLength} and ${
+          config.password.maxLength
+        } characters`
+      );
+    } else {
+      return await argon2.hash(password, { type: argon2id });
+    }
+  } else {
+    return password;
+  }
 }
