e3daa1c5738b3765b322d7e33f146ec5c8547f00
sonercirit <sonercirit@gmail.com>
2019-05-31 20:17:40 +0300

    inits dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import { ApolloServer } from "apollo-server";
 import { createConnection } from "typeorm";
+import dotenv from "dotenv";
 import { resolvers, typeDefs } from "./graphql/root";
+
+dotenv.config();
 
 /* init db connection from ormconfig.json
    we'll ignore the following line from coverage since
